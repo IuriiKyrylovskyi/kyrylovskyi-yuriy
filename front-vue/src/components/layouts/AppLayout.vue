7b17b78a77c2c7ee3c48f4441ec610ac3b78c685
Iurii Kyrylovskyi <iurii712@gmail.com>
2024-04-15 16:36:59 +0300

    fix import error with type.d.ts

---
+++ front-vue/src/components/layouts/AppLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/me">Me</RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView />
+
+    <slot />
+  </main>
+
+  <footer>footer</footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+</style>
